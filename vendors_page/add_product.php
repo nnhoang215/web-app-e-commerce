cc14309c00f5e34050a854220f58534914426636
K_B <85124305+Velvetbin@users.noreply.github.com>
2022-09-09 02:25:18 +0700

    fix add product

--- vendors_page/add_product.php
+++ vendors_page/add_product.php
@@ -23,6 +23,7 @@
                 </div>
             </nav>
         </header>
+        
         <section class="add-product-page">
             <div class="page-path">
                 <p ><a href="view_my_products.html">PRODUCTS</a> / <a href="">ADD PRODUCT</a></p>
@@ -35,10 +36,100 @@
                 <div class="product-form">
                     <h3>PRODUCT DETAILS</h3>
                     <p>Add product details here</p>
-                    <form action="#" method="#">
+                    <form method="post">
+                        <?php 
+                        
+                        function clean_text($string)
+                        {
+                         $string = trim($string);
+                         $string = stripslashes($string);
+                         $string = htmlspecialchars($string);
+                         return $string;
+
+                        }
+                        $username = '';
+                        $product_name = '';
+                        $price = '';
+                        $product_type = '';
+                        $description = '';
+                        $error = '';
+                        if(isset($_POST['submit'])){
+                            if(empty($_POST["username"]))
+                            {
+                            $error .= '<p><label class="text-danger">Please Enter your Username</label></p>';
+                            }
+                            else
+                            {
+                            $username = clean_text($_POST["username"]);
+                            if(empty($_POST["product-name"]))
+                            {
+                            $error .= '<p><label class="text-danger">Please Enter your Product name</label></p>';
+                            }
+                            else
+                            {
+                            $product_name = clean_text($_POST["product-name"]);
+                            }
+                            if(empty($_POST["price"]))
+                            {
+                            $error .= '<p><label class="text-danger">Price is required</label></p>';
+                            }
+                            else
+                            {
+                            $price = clean_text($_POST["price"]);
+                            if(!preg_match("[0-9]",$price)){
+                            $error .= '<p><label class="text-danger">Only number is allowed</label></p>';          
+                            }
+                            }
+                            if(empty($_POST["product-type"]))
+                            {
+                            $error .= '<p><label class="text-danger">MProduct type is required</label></p>';
+                            }
+                            else
+                            {
+                            $product_type = clean_text($_POST["product-type"]);
+                            }
+                            if(empty($_POST["description"]))
+                            {
+                            $error .= '<p><label class="text-danger">Description is required</label></p>';
+                            }
+                            else
+                            {
+                            $description = clean_text($_POST["description"]);
+                            }
+
+                            if($error == '')
+                            {
+                            
+                            $file_open = fopen('Product.csv', 'a');
+                            
+                            $form_data = array(
+                            'productID' => 1,
+                            'name'  => $product_name,
+                            'type'  => $product_type,
+                            'description'  => $description,
+                            'imagefileName' => null,
+                            'price' => $price,
+                            'vendorUsername' => $username
+                            );
+
+                            fputcsv($file_open, $form_data) or die('<script>console.log("unsuccessful");</script>');
+                            fclose($file_open);
+                            $error = '<label class="text-success">Thank you for contacting us</label>';
+                            $username = '';
+                            $product_name = '';
+                            $price = '';
+                            $product_type = '';
+                            $description = '';
+                            
+                            }
+                           }
+                         }
+                        
+                        
+                        ?>
                         <div class="contain-input">
                             <label for="username">VENDOR USERNAME <span>*</span></label> <br>
-                            <input type="text" name="username" id="username" onkeyup="validateName()">
+                            <input type="text" name="username" id="username" onkeyup="validateName()" >
                             <span id="nameError"></span>
                         </div>
                             <div class="contain-input">
@@ -61,21 +152,26 @@
                             <textarea name="desciption" id="desciption" cols="60" rows="10" onkeyup="validatepDescription()"></textarea>
                             <span id="desciptionError"></span> <br>
                         </div>
-                    </form>
-                </div>
-                <div class="product-img-input">
-                    <div class="chosen-file" id="imgBox">
-                        <p>No file chosen</p>
+
+                    <div class="product-img-input">
+                        <div class="chosen-file" id="imgBox">
+                            <p>No file chosen</p>
+                        </div>
+                        <h4>Note:</h4>
+                        <p>Image can be uploaded of any dimension but we reccomend you to 
+                            upload image with dimension of 1024x1024 & its size must be less than 15MB.
+                        </p>
+                     </div>
+
+                    <div class="form-group">
+                        <input type="submit" name="submit" class="btn btn-info" value="Submit" />
                     </div>
-                    <h4>Note:</h4>
-                    <p>Image can be uploaded of any dimension but we reccomend you to 
-                        upload image with dimension of 1024x1024 & its size must be less than 15MB.
-                    </p>
+
+                </form>
                 </div>
+                
             </div>
-            <div class="form-group">
-                <input type="submit" name="submit" class="save-changes" value="Upload" />
-             </div>
+            
         </section>
         <footer>
             <ul>
